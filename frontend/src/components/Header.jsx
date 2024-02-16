f6b5e5b5145543e2d0874ab559f469473938c2fd
Yashraj09 <sorathiyayash1231@gmail.com>
2024-02-16 11:01:35 +0530

    Implemented React Router

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,28 +1,39 @@
-import React from 'react';
-import { Navbar, Nav, Container, NavLink} from 'react-bootstrap';
-import {FaShoppingCart, FaUser } from 'react-icons/fa';
-import logo from '../assets/logo.png'
+import React from "react";
+import { Navbar, Nav, Container, NavLink } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
-        //altogether, this line of code creates a navbar with a dark background and light text, which expands to a full-width layout on large screens and automatically collapses the mobile menu when a menu item is selected.
+    //altogether, this line of code creates a navbar with a dark background and light text, which expands to a full-width layout on large screens and automatically collapses the mobile menu when a menu item is selected.
     <header>
-        <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-            <Container>
-                <Navbar.Brand href='/'>
-                    <img src={logo} alt="PragatiDukaan" />
-                    PragatiDukaan
-                    </Navbar.Brand>
-                <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-                <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className="ms-auto">
-                    <NavLink href='/cart'><FaShoppingCart/> Cart</NavLink>
-                    <NavLink href='/login'><FaUser /> Sign In</NavLink>
-                    </Nav>
-                </Navbar.Collapse>
-                </Container>     
-        </Navbar>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="PragatiDukaan" />
+              PragatiDukaan
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <NavLink>
+                  <FaShoppingCart /> Cart
+                </NavLink>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <NavLink >
+                  <FaUser /> Sign In
+                </NavLink>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
-  )
-}
+  );
+};
 export default Header;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import 'bootstrap/dist/css/bootstrap.min.css' 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+// import 'bootstrap/dist/css/bootstrap.min.css'
 //bootstrap.min.css: This is the actual CSS file you're importing. It's a minimized version (minified) of the Bootstrap CSS, meaning it has been compressed to reduce its file size for faster loading on web pages.
-import './assets/styles/bootstrap.custom.css'
-import './assets/styles/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import HomeScreen from "./screens/HomeScreen";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />}/>
+    </Route>
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
