a9e4a89635a8c2d2c4993b8be8ce89a91b2e8adc
Yashraj09 <sorathiyayash1231@gmail.com>
2024-02-20 13:06:17 +0530

    added mongodb URI

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -6,14 +6,24 @@ import Products from "../components/Products";
 
 const HomeScreen = () => {
   const [products, setProducts] = useState([]);
+  // using the useState hook to initialize a state variable named products with an empty array as its initial value. The setProducts function is a setter function provided by the useState hook, which is used to update the value of the products state variable.
+
 
   useEffect(() => {
+    // useEffect hook is used to perform side effects in functional components, such as data fetching, subscriptions, or DOM manipulation. In this case, you're using it to fetch products data from the server when the component mounts.
     const fetchProducts = async () => {
+
+
       const { data } = await axios.get("/api/products");
       // The response object typically contains various properties such as data, status, statusText, headers, etc. The data property specifically holds the response data returned by the server.
 
+      // This function sends an HTTP GET request to the /api/products endpoint using Axios
+      // When the request resolves successfully, Axios returns a response object.
+
       // By using object destructuring syntax { data }, you're extracting the data property from the response object and storing it in a variable named data
       setProducts(data);
+
+      // Finally, you're updating the products state variable with the response data obtained from the server. This triggers a re-render of the component with the updated products state, causing any UI elements dependent on products to reflect the fetched data.
     };
 
     fetchProducts();
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import { useState, useEffect} from 'react';
-import { useParams} from 'react-router-dom'
-import {Link} from 'react-router-dom'
-import {Row, Col, Image, ListGroup, Card, Button, ListGroupItem} from 'react-bootstrap'
-import Rating from '../components/Rating'
-import axios from 'axios'
-
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+} from "react-bootstrap";
+import Rating from "../components/Rating";
+import axios from "axios";
 
 const ProductScreen = () => {
   const [product, setProduct] = useState([]);
 
-    const { id: productId } = useParams();
-    useEffect(()=>{
-      const fetchProduct = async()=>{
-        const {data} = await axios.get(`/api/products/${productId}`)
-        setProduct(data)
-      };
+  const { id: productId } = useParams(); //useParams() is a custom hook provided by React Router that allows functional components to access the parameters from the URL.
+  // { id: productId }, you're extracting the value of the id parameter from the URL and storing it in a variable named productId.
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
 
-      fetchProduct();
-    },[productId])
+    fetchProduct();
+  }, [productId]);
 
   return (
     <>
@@ -90,6 +98,6 @@ const ProductScreen = () => {
       </Row>
     </>
   );
-}
+};
 
-export default ProductScreen
+export default ProductScreen;
