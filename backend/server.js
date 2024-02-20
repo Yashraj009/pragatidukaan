4e7a61c403977d5ee123167afc020d1e6084cf30
Yashraj09 <sorathiyayash1231@gmail.com>
2024-02-20 10:57:34 +0530

    started api with products.js and added some dev dependencies

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+// we can use this as es6 module is type. it has been modified in package.json
+//default importing is like 
+// const express = require('express')
+import products from "./data/products.js";
+const port = 5000;
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send("API is running");
+})
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id",(req,res)=>{
+    const product = products.find((p)=> p._id === req.params.id);
+    res.json(product)
+})
+
+app.listen(port,()=>{`API is running at port ${port}`})
