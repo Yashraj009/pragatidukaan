c93337982f36dad6c81469158b5928b6b70afd2c
Yashraj09 <sorathiyayash1231@gmail.com>
2024-02-20 12:08:56 +0530

    fetch data from backend products.js using axios

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Row, Col } from "react-bootstrap";
-import products from "../products";
 import Products from "../components/Products";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      // The response object typically contains various properties such as data, status, statusText, headers, etc. The data property specifically holds the response data returned by the server.
+
+      // By using object destructuring syntax { data }, you're extracting the data property from the response object and storing it in a variable named data
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
   return (
     <>
       <h1>Latest Products</h1>
